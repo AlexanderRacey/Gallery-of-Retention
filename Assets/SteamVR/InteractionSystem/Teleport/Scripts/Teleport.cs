f38e7a3ace585674d65aa0908d372e4934223044
Alexander Racey <alexander.racey@gmail.com>
2020-11-30 13:05:17 +0000

    ShowHints updated

--- Assets/SteamVR/InteractionSystem/Teleport/Scripts/Teleport.cs
+++ Assets/SteamVR/InteractionSystem/Teleport/Scripts/Teleport.cs
@@ -15,6 +15,9 @@ namespace Valve.VR.InteractionSystem
     {
         public SteamVR_Action_Boolean teleportAction = SteamVR_Input.GetAction<SteamVR_Action_Boolean>("Teleport");
 
+		// Show teleport hints
+		public bool showHints = false;
+
         public LayerMask traceLayerMask;
 		public LayerMask floorFixupTraceLayerMask;
 		public float floorFixupMaximumTraceDistance = 1.0f;
@@ -188,7 +191,11 @@ namespace Valve.VR.InteractionSystem
 
 			CheckForSpawnPoint();
 
-			Invoke( "ShowTeleportHint", 5.0f );
+            // Show hints if enabled
+            if (showHints)
+            {
+				Invoke("ShowTeleportHint", 5.0f);
+			}
 		}
 
 
