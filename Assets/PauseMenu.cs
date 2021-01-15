88d5301315d5b42305410e1efc9c498e0646955a
Alexander Racey <alexander.racey@gmail.com>
2021-01-15 18:14:33 +0000

    Added pause menu to nonVR scene

---
+++ Assets/PauseMenu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// NonVR pause menu
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    // Disable pause menu at start of game
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1.0f;
+        gameIsPaused = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0.0f;
+        gameIsPaused = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1.0f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
+    }
+}
--- Assets/Scripts/LoadNonVRScene.cs
+++ Assets/Scripts/LoadNonVRScene.cs
@@ -9,5 +9,6 @@ public class LoadNonVRScene : MonoBehaviour
     void Start()
     {
         SceneManager.LoadSceneAsync("NonVRScene");
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
