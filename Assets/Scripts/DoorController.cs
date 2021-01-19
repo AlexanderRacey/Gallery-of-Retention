771be1d40a80d5cc051e6561904d188269b2df9e
Alexander Racey <alexander.racey@gmail.com>
2021-01-19 14:37:54 +0000

    Button opens door (nonVR)

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Open door (nonVR)
+public class DoorController : MonoBehaviour
+{
+    public GameObject Door;
+    public bool doorIsOpening;
+
+    void Update()
+    {
+        if (doorIsOpening == true)
+        {
+            Door.transform.Translate(Vector3.up * Time.deltaTime * 5);
+        }
+        if (Door.transform.position.y > 10.0f)
+        {
+            doorIsOpening = false;
+        }
+    }
+
+    // Open door on button click
+    void OnMouseDown()
+    {
+        doorIsOpening = true;
+    }
+}
