2668d6127054f74c59c1b00b3fb57a69622cb956
Alexander Racey <alexander.racey@gmail.com>
2021-02-28 15:57:51 +0000

    VR door opens with button push, need to get it to start timer

---
+++ Assets/Scripts/VRDoorController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR.InteractionSystem;
+using UnityEngine.UI;
+
+// Open door using button in VR
+public class VRDoorController : MonoBehaviour
+{
+    public GameObject Door;
+    public bool vrDoorOpening = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnPress(Hand hand)
+    {
+        OpenDoor();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(vrDoorOpening == true)
+        {
+            Door.transform.Translate(Vector3.up * Time.deltaTime * 5);
+        }
+    }
+
+    void OpenDoor()
+    {
+        vrDoorOpening = true;
+    }
+}
---
+++ Assets/Scripts/VRTimerUpdater.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class VRTimerUpdater : MonoBehaviour
+{
+    public GameObject doorButton;
+    public Animator transition;
+    public float transitionTime = 1.0f;
+
+    private TextMeshPro textMesh;
+    private float time;
+    private bool timerCounting;
+
+    void Start()
+    {
+        // Total time displayed in seconds
+        textMesh = GetComponent<TextMeshPro>();
+
+        //time = 21.0f;
+        time = 601.0f;
+        timerCounting = false;
+    }
+
+    // Start timer when door opens
+    void Update()
+    {
+        if (doorButton.GetComponent<VRDoorController>().vrDoorOpening == true)
+        {
+            timerCounting = true;
+        }
+
+        if (timerCounting)
+        {
+            string minSec = string.Format("{0}:{1:00}", (int)time / 60, (int)time % 60);
+            textMesh.text = minSec;
+            time -= Time.deltaTime;
+
+            if (time < 0.0f)
+            {
+                LoadLevel();
+                SceneManager.LoadSceneAsync("ClosingScene");
+            }
+        }
+    }
+    IEnumerator LoadLevel()
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(transitionTime);
+    }
+}
