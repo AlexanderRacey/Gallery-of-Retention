59a83ee28dedd7c8944d8b363b4bb2eac713ed41
Alexander Racey <alexander.racey@gmail.com>
2021-03-02 20:04:06 +0000

    Game ready for testing, installer to be created, merge with master

---
+++ Assets/Scripts/VRSelectionEnabler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class VRSelectionEnabler : MonoBehaviour
+{
+    public GameObject vrExperienceButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (OpenVR.IsHmdPresent())
+        {
+            vrExperienceButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            vrExperienceButton.gameObject.SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OpenVR.IsHmdPresent())
+        {
+            vrExperienceButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            vrExperienceButton.gameObject.SetActive(false);
+        }
+    }
+}
